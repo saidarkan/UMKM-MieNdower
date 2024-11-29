6abd8251424ce9140d10a4cc9b9c70f7a9515d43
saidarkan <said23ti@mahasiswa.pcr.ac.id>
2024-11-29 12:49:39 +0700

    Menambahkan migrasi tabel menu

--- database/migrations/2024_11_29_032234_create_menus_table.php
+++ database/migrations/2024_11_29_032234_create_menus_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nama_menu');
+            $table->string('jenis_menu');
+            $table->text('deskripsi_menu');
+            $table->integer('harga_menu');
+            $table->binary('gambar_menu');
         });
     }
 
