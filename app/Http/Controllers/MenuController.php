42c6e264c9913ea63ed6a37d6a6d3b90480a4c4a
saidarkan <said23ti@mahasiswa.pcr.ac.id>
2024-11-29 10:23:12 +0700

    Penghapusan controller sebelumnya dan penambahan controller baru

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MenuProduk;
+use App\Models\Menu;
 use Illuminate\Http\Request;
 
-class MenuProdukController extends Controller
+class MenuController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -34,7 +34,7 @@ class MenuProdukController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(MenuProduk $menuProduk)
+    public function show(Menu $menu)
     {
         //
     }
@@ -42,7 +42,7 @@ class MenuProdukController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(MenuProduk $menuProduk)
+    public function edit(Menu $menu)
     {
         //
     }
@@ -50,7 +50,7 @@ class MenuProdukController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, MenuProduk $menuProduk)
+    public function update(Request $request, Menu $menu)
     {
         //
     }
@@ -58,7 +58,7 @@ class MenuProdukController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(MenuProduk $menuProduk)
+    public function destroy(Menu $menu)
     {
         //
     }
