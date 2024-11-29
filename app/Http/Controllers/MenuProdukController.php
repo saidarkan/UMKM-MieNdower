4d2e13a267acbf5a76ed1b5a4b7873ea3d3de1ce
saidarkan <said23ti@mahasiswa.pcr.ac.id>
2024-11-29 08:11:20 +0700

    Menambahkan controller menu produk

---
+++ app/Http/Controllers/MenuProdukController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MenuProduk;
+use Illuminate\Http\Request;
+
+class MenuProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MenuProduk $menuProduk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MenuProduk $menuProduk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MenuProduk $menuProduk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MenuProduk $menuProduk)
+    {
+        //
+    }
+}
