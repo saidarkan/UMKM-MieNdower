9d98bdbf86812cdcc4e9029d72d0120ec9897ae8
saidarkan <said23ti@mahasiswa.pcr.ac.id>
2025-01-20 05:19:58 +0700

    Memperbaiki Model Homepage

--- app/Models/Homepage.php
+++ app/Models/Homepage.php
@@ -8,18 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Homepage extends Model
 {
     use HasFactory;
+    
 
     // Nama tabel dalam database
-    protected $table = 'homepage';
+    protected $table = 'homepages';
 
     // Kolom yang dapat diisi melalui mass assignment
     protected $fillable = [
-        'judul',
+        'pembuka',
+        'judul1',
+        'judul2',
         'deskripsi_judul',
         'subjudul',
         'deskripsi_subjudul',
         'gambar',
     ];
 
-    public $timestamps = false; // Ubah ke true jika tabel memiliki kolom timestamps
+    // Menonaktifkan timestamps jika tabel tidak memiliki kolom created_at dan updated_at
+    public $timestamps = false;
 }
